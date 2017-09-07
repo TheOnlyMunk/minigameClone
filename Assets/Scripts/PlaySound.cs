800b9a9fa6dca7f5c297a1bbfbeaa5d9a31dbc73
JesterJ <jesterj00@gmail.com>
2017-09-07 14:21:57 +0200

    Sound and Color Script seperated
    Color script now in script alone and soundscript likewise

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    private VRStandardAssets.Utils.EyeSelect m_EyeSelect;
+    [SerializeField] private AudioClip m_OnFilledClip;
+    [SerializeField] private AudioSource m_Audio;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnEnable()
+    {
+        m_EyeSelect = this.GetComponent<VRStandardAssets.Utils.EyeSelect>();
+        m_EyeSelect.OnSelected += PlayAudio;
+    }
+
+    private void OnDisable()
+    {
+        m_EyeSelect.OnSelected -= PlayAudio;
+    }
+
+    private void PlayAudio()
+    {
+        m_Audio.clip = m_OnFilledClip;
+        m_Audio.Play();
+    }
+}
