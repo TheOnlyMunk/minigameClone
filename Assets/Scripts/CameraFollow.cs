a47baa80bfdce3d033615709c9f5d6d70da2afa9
Jens Thomsen <dnamesnej@gmail.com>
2017-09-08 09:29:08 +0200

    Camera follows works

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,34 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-
-	public Transform FollowMe;
-    public Transform Parrent;
     
     private VRStandardAssets.Utils.EyeSelect m_EyeSelect;
 
     Coroutine m_Following;
 
 	public bool m_Follow;
-	private bool canDrag = true;
+	private bool canSelect = true;
     
-    public float speed = 30.0f;
+	public bool rotateTowardsPlayer = true;
+	public float rotationSpeed = 10.0f;
+	public float followSpeed = 10.0f;
 	public float detachRange = 5.0f;
+	public float distanceFromPlayer = 10f;
+	public float shakeHeadCooldown = 1f;
+	public float maxHeight = 10f;
 
-	public Transform pusher;
+	public Transform followObject;
+	private Rigidbody followRigid;
 
 	public Vector3 newPos; 
 	private Vector3 endPoint;
 
-	private Rigidbody pullRigid;
-
 	void Start(){
-		// 
-		pullRigid = this.gameObject.transform.GetComponent<Rigidbody> ();
-		FollowMe = Instantiate (FollowMe, this.transform.position, Quaternion.identity);
-		pusher = this.gameObject.transform;
+		// assign rigidbody and gameobject of the follow object
+		followRigid = this.gameObject.transform.GetComponent<Rigidbody> ();
+		followObject = this.gameObject.transform;
 	}
-
 
     private void OnEnable()
     {
@@ -46,13 +45,14 @@ public class CameraFollow : MonoBehaviour {
 
     private void Selected()
     {
-		if (canDrag) {
-			//FollowMe.transform.position = this.transform.position;
-			//FollowMe.transform.parent = Parrent;
-			endPoint =  pusher.transform.position;
-			// Create object at the position of the Object, and set it as a child of the Camera.
+		// make sure the object cannot be selected right after a headshake and while it following
+		if (canSelect && !m_Follow) {
+			// set gravity of follow object to false, so this won't be taken into account when following
+			followRigid.useGravity = false;
+			// set the initial endpoint as the objects position, so it is certain that it won't be dropped because of detachRange
+			endPoint = followObject.transform.position;
 
-			// Start a coroutine to follow the object 
+			// Start a coroutine to make the object follow the camera movement
 			m_Following = StartCoroutine (Following ());
 		}
     }
@@ -63,22 +63,36 @@ public class CameraFollow : MonoBehaviour {
         m_Follow = true;
         while (m_Follow)
         {
-			
-			if (Vector3.Distance (endPoint, pusher.transform.position) < 4) {
+			// If the distance between the follow object and the endpoint is greater than detach range, it will be dropped
+			if (Vector3.Distance (endPoint, followObject.transform.position) < detachRange) {
+				
+				// Calculate the position of the object
 				newPos = new Vector3 (Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
 				newPos = Vector3.Normalize (newPos);
 				float angle = Vector3.Angle (newPos, Camera.main.transform.forward);
+				//translate degrees to radians
 				angle = (angle * Mathf.PI) / 180;
-				float hypLength = Mathf.Abs (10f / Mathf.Cos (angle));
+				float hypLength = Mathf.Abs (distanceFromPlayer / Mathf.Cos (angle));
 				endPoint = Camera.main.transform.position + (Camera.main.transform.forward.normalized * hypLength);
 
+				// if the endpoint if bigger than maxheight, the y coordinate of endpoint is locked at maxheight
+				if (endPoint.y > maxHeight) {
+					endPoint = new Vector3 (endPoint.x, maxHeight, endPoint.z);
+				}
+
+				if (rotateTowardsPlayer) {
+					float step = rotationSpeed * Time.deltaTime;
+					transform.rotation = Quaternion.RotateTowards(transform.rotation, Camera.main.transform.rotation, step);
+				}
+
 				// Addforce stuff
-				pullRigid.velocity = Vector3.zero;
-				pullRigid.angularVelocity = Vector3.zero;
-				pullRigid.AddForce ((endPoint - pusher.transform.position) * speed);
+				followRigid.velocity = Vector3.zero;
+				followRigid.angularVelocity = Vector3.zero;
+				// Addforce in the direction of endpoint
+				followRigid.AddForce ((endPoint - followObject.transform.position) * followSpeed);
+
 			}else {
 				m_Follow = false;
-
 			}
 				
             // Wait until next frame.
@@ -88,23 +102,19 @@ public class CameraFollow : MonoBehaviour {
             if (m_Follow)
                 continue;
 
-            // The object stopped following
-			//Destroy(FollowMe.gameObject);
-			//pusher.GetComponent<Rigidbody> ().velocity = Vector3.zero;
-			//pusher.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
-			//pusher.GetComponent<Rigidbody> ().Sleep();
-			pusher.GetComponent<Rigidbody> ().useGravity = true;
-			FollowMe.transform.parent = null;
+			// if it should not follow
+			// use gravity to make the object fall down
+			followRigid.useGravity = true;
             yield break;
         }
     }
 
-
+	// cooldown, so that a dropped object by headshake can't be picked up right away
 	public IEnumerator CoolDown()
 	{
-		canDrag = false;
-		yield return new WaitForSeconds (1);
-		canDrag = true;
+		canSelect = false;
+		yield return new WaitForSeconds (shakeHeadCooldown);
+		canSelect = true;
 	}
 
 }
