cedd9938333db5a6957ec2aea4e022e4d2d4806b
Jens Thomsen <dnamesnej@gmail.com>
2017-09-10 21:04:53 +0200

    Update to eyeselect and activation trigger
    Eyeselect can detect when exiting a collider and activation trigger can
    now enable and disable it’s child gameobject + fadein

--- Assets/Scripts/ActivationTrigger.cs
+++ Assets/Scripts/ActivationTrigger.cs
@@ -6,16 +6,50 @@ using UnityEngine.UI;
 public class ActivationTrigger : MonoBehaviour {
 
 	private VRStandardAssets.Utils.EyeSelect m_EyeSelect;
-	public GameObject gameObject;
+	public float fadeInDuration = 1.0f;
+	private bool isActivated = false;
 
 	void OnEnable()
 	{
 		m_EyeSelect = this.GetComponent<VRStandardAssets.Utils.EyeSelect>();
-		m_EyeSelect.OnSelected += DisplayObject;
+		m_EyeSelect.OnSelection += DisplayObject;
+		m_EyeSelect.OnExit += FadeoutObject;
 	}
 
-	void DisplayObject(){
-		if (!gameObject.activeSelf)
-			gameObject.SetActive (true);
+	void OnDisable()
+	{
+		m_EyeSelect.OnSelection -= DisplayObject;
+		m_EyeSelect.OnExit -= FadeoutObject;
+
 	}
+			
+
+	void DisplayObject(){
+		print ("activate");
+		if (!transform.GetChild (0).gameObject.activeSelf) {
+			transform.GetChild (0).gameObject.SetActive (true);
+			StartCoroutine(FadeToFullAlpha(fadeInDuration, transform.GetChild(0).GetComponent<MeshRenderer>().material));
+		}
+	}
+
+	void FadeoutObject(){
+		print ("Deactivate");
+		if (transform.GetChild (0).gameObject.activeSelf) {
+			transform.GetChild (0).gameObject.SetActive (false);
+		}
+
+	}
+
+	public IEnumerator FadeToFullAlpha(float t, Material i)
+	{
+		i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+		while (i.color.a < 1.0f)
+		{
+			i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+			yield return null;
+		}
+	}
+
+
+
 }
