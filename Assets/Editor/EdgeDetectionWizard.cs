e60fce36c09feb8ab9592ce7feab190f7a3a43cc
jensgholm15@gmail.com <jensgholm15@gmail.com>
2017-09-10 20:04:49 +0200

    Add edior tool for edge texture creastion

---
+++ Assets/Editor/EdgeDetectionWizard.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class EdgeDetectionWizard : ScriptableWizard
+{
+    class Uniforms
+    {
+        public static readonly int _Sensitivity = Shader.PropertyToID("_Sensitivity");
+        public static readonly int _BlurDirection = Shader.PropertyToID("_BlurDirection");
+    }
+
+    Shader m_Shader;
+    Shader shader
+    {
+        get
+        {
+            if (!m_Shader)
+            {
+                m_Shader = Shader.Find("Hidden/Liquid Edge Filter");
+            }
+
+            return m_Shader;
+        }
+    }
+
+    Material m_Material;
+    Material material
+    {
+        get
+        {
+            if (!m_Material)
+            {
+                m_Material = new Material(shader);
+            }
+
+            return m_Material;
+        }
+    }
+
+    public Texture2D source;
+
+    [Range(0f, 1f)]
+    public float sensitivity = .5f;
+
+    [MenuItem("Tools/Create liquify edge texture")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard("Create edge texture", typeof(EdgeDetectionWizard), "Create!");
+    }
+
+    void OnWizardUpdate()
+    {
+        helpString = "Generates edge information for a texture";
+
+        if (null == source)
+        {
+            errorString = "Select a source texture";
+            isValid = false;
+        }
+        else
+        {
+            errorString = "";
+            isValid = true;
+        }
+    }
+
+    void OnWizardCreate()
+    {
+        var path = EditorUtility.SaveFilePanel("Save texture as PNG", "", source.name + "-edges.png", "png");
+        Debug.Log(path);
+
+        RenderTexture temporary = RenderTexture.GetTemporary(source.width, source.height, 24, RenderTextureFormat.ARGB32);
+        RenderTexture temporary2 = RenderTexture.GetTemporary(source.width, source.height, 24, RenderTextureFormat.ARGB32);
+        temporary.Create();
+        temporary2.Create();
+
+        Camera activeCamera = Camera.main;
+ 
+        // Initialize and render
+        activeCamera.targetTexture = temporary;
+        activeCamera.Render();
+        RenderTexture.active = temporary;
+
+        Graphics.Blit(source, RenderTexture.active, material, 0);
+
+        for (int i = 0; i < 32; ++i)
+        {
+            material.SetVector(Uniforms._BlurDirection, new Vector4(1f, 0f, 0f, 0f));
+            Graphics.Blit(temporary, temporary2, material, 1);
+
+            material.SetVector(Uniforms._BlurDirection, new Vector4(0f, 1f, 0f, 0f));
+            Graphics.Blit(temporary2, temporary, material, 1);
+        }
+ 
+        Texture2D output = new Texture2D(source.width, source.height, TextureFormat.ARGB32, source.mipmapCount > 0);
+
+        // Read pixels
+        output.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0, true);
+ 
+        // Clean up
+        activeCamera.targetTexture = null;
+        RenderTexture.active = null;
+
+        DestroyImmediate(temporary);
+        DestroyImmediate(temporary2);
+        DestroyImmediate(m_Material);
+
+        byte[] bytes = output.EncodeToPNG();
+        File.WriteAllBytes(path, bytes);
+
+        AssetDatabase.Refresh();
+    }
+
+    protected override bool DrawWizardGUI()
+    {
+        source = (Texture2D) EditorGUILayout.ObjectField("Source texture", source, typeof (Texture2D), false); 
+        sensitivity = EditorGUILayout.Slider ("Sensitivity", sensitivity, 0f, 1f);
+
+        if (source)
+        {
+            Rect rect = GUILayoutUtility.GetRect(512, 512);
+
+            material.SetFloat(Uniforms._Sensitivity, sensitivity);
+            Graphics.DrawTexture(rect, source, material, 0);
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Resources/Shaders/LiquidEdgeFilter.shader
@@ -0,0 +1,104 @@
+﻿Shader "Hidden/Liquid Edge Filter"
+{
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Off Blend Off
+
+        CGINCLUDE
+        #include "UnityCG.cginc"
+
+        sampler2D _MainTex;
+        float4 _MainTex_TexelSize;
+
+        float _Sensitivity;
+        float2 _BlurDirection;
+
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float2 uv : TEXCOORD0;
+        };
+
+        struct Varyings
+        {
+            float2 uv : TEXCOORD0;
+            float2 texcoord : TEXCOORD1;
+            float4 vertex : SV_POSITION;
+        };
+
+        Varyings VertDefault (appdata v)
+        {
+            Varyings o;
+            o.vertex = UnityObjectToClipPos(v.vertex);
+            o.uv = v.uv;
+            o.texcoord = v.uv;
+            return o;
+        }
+
+        float4 GaussianBlurFragment5tap(Varyings i) : SV_Target
+        {
+            float2 offset = _MainTex_TexelSize.xy * _BlurDirection;
+
+            float4 blurred = 0.;
+
+            blurred += tex2D(_MainTex, i.texcoord + offset * -4.) * .091638;
+            blurred += tex2D(_MainTex, i.texcoord + offset *  4.) * .091638;
+
+            blurred += tex2D(_MainTex, i.texcoord + offset * -3.) * .105358;
+            blurred += tex2D(_MainTex, i.texcoord + offset *  3.) * .105358;
+
+            blurred += tex2D(_MainTex, i.texcoord + offset * -2.) * .116402;
+            blurred += tex2D(_MainTex, i.texcoord + offset *  2.) * .116402;
+
+            blurred += tex2D(_MainTex, i.texcoord + offset * -1.) * .123572;
+            blurred += tex2D(_MainTex, i.texcoord + offset *  1.) * .123572;
+
+            float4 centerTap = tex2D(_MainTex, i.texcoord);
+            blurred += centerTap * 0.126063;
+
+            return max(blurred, centerTap);
+        }
+
+        float2 Gradient(float up, float down, float left, float right)
+        {
+            float horizontal = (right - left) / 2.;
+            float vertical = (up - down) / 2.;
+
+            return float2(horizontal, vertical);
+        }
+
+        fixed4 EdgeDetectionAlpha(Varyings i) : SV_Target
+        {
+            float4 offset = float4(1., 1., -1., 0.) * _MainTex_TexelSize.xyxy;
+
+            float up = tex2D(_MainTex, i.uv + offset.wy).a;
+            float down = tex2D(_MainTex, i.uv - offset.wy).a;
+            float left = tex2D(_MainTex, i.uv - offset.xw).a;
+            float right = tex2D(_MainTex, i.uv + offset.xw).a;
+
+            float2 gradient = Gradient(up, down, left, right);
+
+            float4 edge = step(_Sensitivity, length(gradient));
+            edge.a = 1.;
+
+            return edge;
+        }
+        ENDCG
+
+        Pass // 0
+        {
+            CGPROGRAM
+            #pragma vertex VertDefault
+            #pragma fragment EdgeDetectionAlpha
+            ENDCG
+        }
+
+        Pass // 1
+        {
+            CGPROGRAM
+            #pragma vertex VertDefault
+            #pragma fragment GaussianBlurFragment5tap
+            ENDCG
+        }
+    }
+}
