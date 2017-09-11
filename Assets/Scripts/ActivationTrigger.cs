a2aa79cdebb93fa1720f18668a2cf574e5fe2636
Jens Thomsen <dnamesnej@gmail.com>
2017-09-11 10:57:56 +0200

    Update to activation trigger
    Now works with 3d text, fades in and out and also applies to all 3d
    text children of the gameobject the script is attached to.

--- Assets/Scripts/ActivationTrigger.cs
+++ Assets/Scripts/ActivationTrigger.cs
@@ -6,50 +6,96 @@ using UnityEngine.UI;
 public class ActivationTrigger : MonoBehaviour {
 
 	private VRStandardAssets.Utils.EyeSelect m_EyeSelect;
+	private Coroutine lastRoutine = null;
+
 	public float fadeInDuration = 1.0f;
-	private bool isActivated = false;
+	public float waitBeforeDisapear = 1.0f; 
 
 	void OnEnable()
 	{
 		m_EyeSelect = this.GetComponent<VRStandardAssets.Utils.EyeSelect>();
-		m_EyeSelect.OnSelection += DisplayObject;
-		m_EyeSelect.OnExit += FadeoutObject;
+		m_EyeSelect.OnSelection += EnableObject;
+		m_EyeSelect.OnExit += DisableObject;
 	}
 
 	void OnDisable()
 	{
-		m_EyeSelect.OnSelection -= DisplayObject;
-		m_EyeSelect.OnExit -= FadeoutObject;
+		m_EyeSelect.OnSelection -= EnableObject;
+		m_EyeSelect.OnExit -= DisableObject;
 
 	}
 			
 
-	void DisplayObject(){
-		print ("activate");
+	void EnableObject(){
+		if (lastRoutine != null) {
+			StopCoroutine (lastRoutine);
+		}
+		// check if childs are active. If not, then they are activated
 		if (!transform.GetChild (0).gameObject.activeSelf) {
-			transform.GetChild (0).gameObject.SetActive (true);
-			StartCoroutine(FadeToFullAlpha(fadeInDuration, transform.GetChild(0).GetComponent<MeshRenderer>().material));
+			foreach (Transform child in transform)
+			{
+				child.gameObject.SetActive(true);
+			}
 		}
+		// start coroutine to fade-in the child objects 
+		StartCoroutine(FadeToFullAlpha(fadeInDuration, transform.GetChild(0).GetComponent<TextMesh>()));
 	}
 
-	void FadeoutObject(){
-		print ("Deactivate");
+	void DisableObject(){
+		// check if childs are active. If they are, then WaitAndDisapear is executed
 		if (transform.GetChild (0).gameObject.activeSelf) {
-			transform.GetChild (0).gameObject.SetActive (false);
+			lastRoutine = StartCoroutine(WaitAndDisapear(fadeInDuration, waitBeforeDisapear, transform.GetChild(0).GetComponent<TextMesh>()));
 		}
-
 	}
+		
 
-	public IEnumerator FadeToFullAlpha(float t, Material i)
+	// fadein to full alpha for all children 
+	public IEnumerator FadeToFullAlpha(float t, TextMesh i)
 	{
-		i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+		// find all textmeshes in the gameobject and its children
+		Component[] textMeshes = GetComponentsInChildren<TextMesh>();
+
+		// Find the current color of the first child - this will be used to set the initial color and check when the alpha is as wanted
+		i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a);
+
+		// loop through textmeshes and set their color until alpha is 1
 		while (i.color.a < 1.0f)
 		{
-			i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+			foreach (TextMesh j in textMeshes)
+			{
+				j.color = new Color(j.color.r, j.color.g, j.color.b, j.color.a + (Time.deltaTime / t));
+			}
 			yield return null;
 		}
 	}
 
+	// wait and then fadeout. The child gameobject is disabled after fadeout
+	public IEnumerator WaitAndDisapear(float t, float wait, TextMesh i)
+	{
+		yield return new WaitForSeconds (waitBeforeDisapear);
 
+		// find all textmeshes in the gameobject and its children
+		Component[] textMeshes = GetComponentsInChildren<TextMesh>();
+
+		// Find the current color of the first child - this will be used to set the initial color and check when the alpha is as wanted
+		i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a);
+
+		// loop through textmeshes and set their color intil alpha is 0
+		while (i.color.a > 0.0f)
+		{
+			foreach (TextMesh j in textMeshes)
+			{
+				j.color = new Color(j.color.r, j.color.g, j.color.b, j.color.a - (Time.deltaTime / t));
+			}
+
+			yield return null;
+		}
+		// loop through children and disable them
+		foreach (Transform child in transform)
+		{
+			child.gameObject.SetActive(false);
+		}
+
+	}
 
 }
